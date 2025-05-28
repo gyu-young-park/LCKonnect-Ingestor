6c1770c712d7aef61b0dff9f9fd415aec3636be9
gyu-young-park <gyoue200125@gmail.com>
2025-05-28 23:26:28 +0900

    first commit

---
+++ src/main/java/io/github/gyu_young_park/LCKonnect_Ingestor/LcKonnectIngestorApplication.java
@@ -0,0 +1,13 @@
+package io.github.gyu_young_park.LCKonnect_Ingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcKonnectIngestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcKonnectIngestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gyu_young_park/LCKonnect_Ingestor/LcKonnectIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gyu_young_park.LCKonnect_Ingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcKonnectIngestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
