7d71cbf375d40994d9831628def4c0c423b0d08d
gyu-young-park <gyoue200125@gmail.com>
2025-05-30 00:53:37 +0900

    chore: test jsoup crawler and get lck score

--- src/main/java/io/github/gyu_young_park/LCKonnect_Ingestor/controller/CrawlerController.java
+++ src/main/java/io/github/gyu_young_park/LCKonnect_Ingestor/controller/CrawlerController.java
@@ -1,4 +1,5 @@
 package io.github.gyu_young_park.LCKonnect_Ingestor.controller;
+import io.github.gyu_young_park.LCKonnect_Ingestor.crawler.LCKCrawler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,10 @@ import java.util.Map;
 @RequestMapping("/crawler")
 public class CrawlerController {
 
-    @GetMapping("/test")
+    @GetMapping()
     public Map<String, String> test() {
+        LCKCrawler lckCrawler = new LCKCrawler();
+        lckCrawler.crawl();
         Map<String, String> data = new HashMap<>();
         data.put("message", "Hello World");
         return data;
